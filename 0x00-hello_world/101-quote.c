a7c8b67dec619191fe1a087d72e4baa0c7e3fe07
Eunice191 <eunicemensah191@gmail.com>
2023-06-15 17:48:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
-*
-* main - Entry point
-*
-* Return: Always 1 (Success)
+* main- Entry point
+* Description: A program that prints to standard error
+* Return: Always 1 (success)
 */
 int main(void)
 {
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fprintf(stderr,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
