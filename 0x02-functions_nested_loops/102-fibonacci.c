7b94188690d6e9b8e21fb6fb0ac8ac89f3277198
Eunice191 <eunicemensah191@gmail.com>
2023-06-21 22:42:32 -0700

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,40 +1,27 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
-*print_times_table - Printing the times table of the input,
-*starting with 0.
-*@n: the value of the times table to print.
+*main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+*separated by a comma followed by a space
+*
+*Return: Always 0.
 */
-void print_times_table(int n)
+int main(void)
 {
-	int num, mult, prod;
+		int count;
+		unsigned long fib1 = 0, fib2 = 1, sum;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (num = 0; num <= n; num++)
+		for (count = 0; count < 50; count++)
 		{
-			_putchar('0');
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
-				prod = num * mult;
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
+			sum = fib1 + fib2;
+			printf("%lu", sum);
+			fib1 = fib2;
+			fib2 = sum;
+
+			if (count == 49)
+				printf("\n");
+			else
+				printf(", ");
 		}
-	}
+		return (0);
 }
