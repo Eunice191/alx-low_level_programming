c4c684e4906bc8c78a92e031377812ea540de1c0
Eunice191 <eunicemensah191@gmail.com>
2023-06-27 09:41:36 -0700

    task 0

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * swap_int - swaps the values of two integers
- * @a: integer to swap
- * @b: integer to swap
- */
+*swap_int - swaps the values of two integers
+*@a: integer to swap
+*@b: integer to swap
+*Return: always 0
+*/
 void swap_int(int *a, int *b)
 {
 	int m;
@@ -12,3 +14,4 @@ void swap_int(int *a, int *b)
 	*a = *b;
 	*b = m;
 }
+
