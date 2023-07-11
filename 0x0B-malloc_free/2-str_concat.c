6632dba1db4290346fa518cb77c250d8ac90d5dd
Eunice191 <eunicemensah191@gmail.com>
2023-07-11 13:45:19 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,26 +3,38 @@
 #include <stdio.h>
 
 /**
-*_strdup - a program that returns a pointer to a newly allocated
-*space in memory, which contains a copy of the string given as a parameter.
-*@str: string
+*str_concat - a program that concatenates two strings
+*@s1: first string character
+*@s2: second string character
 *Return: address of the newly allocated memory
 */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	char *bin;
-	int x, root = 0;
+	int m, n, bin1, bin2, bin;
+	char *paste;
 
-	if (str == NULL)
+	bin1 = bin2 = 0;
+	if (s1 != NULL)
+	{
+		m = 0;
+		while (s1[m++] != '\0')
+			bin1++;
+	}
+	if (s2 != NULL)
+	{
+		m = 0;
+		while (s2[m++] != '\0')
+			bin2++;
+	}
+	bin = bin1 + bin2;
+	paste = (char *)malloc(sizeof(char) * (bin + 1));
+	if (paste == NULL)
 		return (NULL);
-	while (str[root] != '\0')
-		root++;
-	bin = (char *)malloc((sizeof(char) * root) + 1);
-	if (bin == NULL)
-		return (NULL);
-	for (x = 0; x < root; x++)
-		bin[x] = str[x];
-	bin[root] = '\0';
+	for (m = 0; m < bin1; m++)
+		paste[m] = s1[m];
+	for (n = 0; n < bin2; n++, m++)
+		paste[m] = s2[n];
+	paste[bin] = '\0';
 
-	return (bin);
+	return (paste);
 }
