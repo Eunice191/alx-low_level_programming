2ec74a2e59933f458fa8bbe98e977dfa916a5ada
Eunice191 <eunicemensah191@gmail.com>
2023-07-17 16:28:08 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,17 +1,49 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- *free_dog - a program that frees dog
- *@d: pointer
- *Return: void
+ *new_dog - a program that creates a new dog
+ *@name: Poppy
+ *@age: 3.5
+ *@owner: Bob
+ *Return: pointer to new dog
  */
-void free_dog(dog_t *d)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d)
+	int i = 0, j = 0, k;
+	dog_t *doge;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	doge = malloc(sizeof(dog_t));
+	if (doge == NULL)
 	{
-		free(d->name);
-		free(d->owner);
-		free(d);
+		free(doge);
+		return (NULL);
 	}
+	doge->name = malloc(i * sizeof(doge->name));
+	if (doge->name == NULL)
+	{
+		free(doge->name);
+		free(doge);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		doge->name[k] = name[k];
+	doge->age = age;
+	doge->owner = malloc(j * sizeof(doge->owner));
+	if (doge->owner == NULL)
+	{
+		free(doge->owner);
+		free(doge->name);
+		free(doge);
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+		doge->owner[k] = owner[k];
+	return (doge);
+
 }
