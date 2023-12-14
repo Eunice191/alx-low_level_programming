bd755e355243d3ead0d0ebaef4e5c45ba8cbe55a
Eunice191 <eunucemensah191@gmail.com>
2023-12-14 10:07:16 -0800

    task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Compile all .c files in the current directory into object files
-gcc -c -fPIC *.c
-# Create the dynamic library liball.so from the object files
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
-# Clean up by removing the object files
-rm *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
